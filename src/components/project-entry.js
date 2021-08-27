4fe988be369696da63889803921a4b347b2ad662
RicoVDK <83656086+RicoVDK@users.noreply.github.com>
2021-08-27 23:57:02 +0200

    Fix hover glow on project entry

--- src/components/project-entry.js
+++ src/components/project-entry.js
@@ -22,7 +22,7 @@ const ProjectEntry = ({
 		>
 			<div
 				id={id}
-				className={`relative flex flex-col w-full h-full bg-gray-800 hover:bg-gray-700 border border-gray-700 rounded-3xl items-center m-auto overflow-hidden group ${buttonScaleHover}`}
+				className={`relative flex flex-col w-full h-full bg-gray-800 border border-gray-700 rounded-3xl items-center m-auto overflow-hidden group ${buttonScaleHover}`}
 			>
 				{/** Image. */}
 				<div className={`relative w-full h-56 overflow-hidden bg-black border-b border-gray-700 text-primary-3`}>
@@ -32,7 +32,7 @@ const ProjectEntry = ({
 						onLoad={(event) => event.target.style.opacity = 1}
 					/>
 
-					<div className={`absolute py-2 px-4 bottom-0 bg-gray-800 group-hover:bg-gray-700 border-t border-r border-gray-700 font-bold rounded-tr-3xl`}>
+					<div className={`absolute py-2 px-4 bottom-0 bg-gray-800 border-t border-r border-gray-700 font-bold rounded-tr-3xl`}>
 						{title}
 					</div>
 				</div>
@@ -49,6 +49,9 @@ const ProjectEntry = ({
 						This entry is currently unavailable
 					</div>
 				</div>}
+
+				{/** Glow. */}
+				{!disabled && <div className={`absolute w-full h-full bg-white opacity-0 group-hover:opacity-10`} />}
 			</div>
 		</Link>
 	)
